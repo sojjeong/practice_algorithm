92182a33979c6f44b06c5101c29879cc1b55db8a
Sojjeong <tip9004@gmail.com>
2021-08-03 13:44:26 +0900

    upload swea1208

---
+++ swea1208.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+	
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int dump = sc.nextInt();
+            List<Integer> arr = new ArrayList<>();
+            
+            for(int i = 0; i < 100; i++){
+                arr.add(sc.nextInt());
+            }       
+
+            for(int i = 0; i < dump; i++){
+                Collections.sort(arr);    
+                arr.set(0, arr.get(0) + 1);
+                arr.set(99, arr.get(99) - 1);
+            }
+            
+            Collections.sort(arr);  
+            int result = arr.get(99) - arr.get(0);
+            System.out.printf("#%d %d%n", test_case, result);
+
+		}
+
+        sc.close();
+        sc = null;
+        
+	}
+}
