c359d26226635dff7273057244529fa97c7b5481
Sojjeong <tip9004@gmail.com>
2022-03-09 23:45:14 +0900

    Upload: 22.03.09

--- Python/BOJ/BOJ1956.py
+++ Python/BOJ/BOJ1956.py
@@ -1,7 +1,7 @@
 import sys
 v, e = map(int, input().split())
 
-INF = 10001
+INF = 400 * 10000 + 1
 
 board = [[INF for _ in range(v)] for _ in range(v)]
 
@@ -20,8 +20,9 @@ for k in range(v):
             if board[i][j] > board[i][k] + board[k][j]:
                 board[i][j] = board[i][k] + board[k][j]
 
-            if i != j != k and board[i][j] != INF and board[j][i] != INF and board[i][k] != INF and board[k][j] != INF and board[j][i] != INF:    
-                min_result = min(min_result, board[i][j] + board[j][i], board[i][k] + board[k][j] + board[j][i])
+            if i != j != k and board[i][j] != INF and board[j][i] != INF and board[i][k] != INF and board[k][j] != INF and board[j][i] != INF:
+                min_result = min(
+                    min_result, board[i][j] + board[j][i], board[i][k] + board[k][j] + board[j][i])
 
 if min_result == sys.maxsize:
     print(-1)
